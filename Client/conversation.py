2c146befb51a3d22b1b9de13f22ded6fb1af9508
emmakennelly <emma.f.kennelly.19@dartmouth.edu>
2017-12-06 13:43:54 +0100

    cleaned up code

--- Client/conversation.py
+++ Client/conversation.py
@@ -45,6 +45,7 @@ class Conversation:
         self.public_keys = {}
         self.conversation_key = ""
         self.my_login_time = datetime.datetime.now()
+        self.curr_time = self.my_login_time
 
     def append_msg_to_process(self, msg_json):
         '''
@@ -125,77 +126,64 @@ class Conversation:
         conversation_key = get_random_bytes(32)
         self.conversation_key = conversation_key
 
-
         my_name = str(self.manager.user_name)
-
-        print my_name + "changed the conversation key to " + self.conversation_key
 
         my_key_file = open("priv_key_" + my_name, "r")
         my_private_key = RSA.importKey(my_key_file.read())
 
-        # print my_private_key.can_encrypt()
-        # print my_private_key.can_sign()
-        # print my_private_key.exportKey()
-
         user_list = self.manager.get_other_users()
 
-        # get public keys of all users
+        # import public keys of all users
+        # public keys will now be stored in self.public_keys[]
 
-        for user in user_list:
-            public_key_file = open("pub_key_" + user, "r")
-            pub_key =  RSA.importKey(public_key_file.read())
-            self.public_keys[user] = pub_key
+        self.get_pub_keys_from_files(user_list)
 
+        # timestamp for key generation is the time of this user's login
         timestamp = str(self.my_login_time)
 
         for user in user_list:
 
-                # encrypt user Alice and session key with Bob's public key
-                my_name = str(self.manager.user_name)
+                # Alice = current user
+                # Bob = user for which Alice is creating key protocol message
+
+                # encrypt Alice's username and session key with Bob's public key
                 total_message = my_name + conversation_key
-                print "KEY MESSAGE: " + total_message
 
                 # get public key of Bob
                 curr_pub_key = self.public_keys[user]
+
                 # create cipher with Bob's public key
                 cipher = PKCS1_OAEP.new(curr_pub_key)
                 encrypted_msg = base64.b64encode(cipher.encrypt(total_message))
 
-                print "length of enc msg: " + str(len(encrypted_msg))
-                print encrypted_msg
-
-                # sign (B | Ta | PubEncKa(A | K) )
+                # sign (B | Ta | PubEncKb(A | K) )
 
                 # sign the message
                 signer = PKCS1_PSS.new(my_private_key)
                 h = SHA.new()
 
                 msg_to_sign = str(user) + timestamp + encrypted_msg
-                print "data to sign " + msg_to_sign
-
                 # sign
                 new_data = self.md5(msg_to_sign)
                 h.update(new_data)
                 signature = signer.sign(h)
 
+
+                # Send to Bob: (Ta | EncKb(A | K) | SigKa(B|Ta|PubEncKb(A|K)) )
                 final_msg = timestamp + encrypted_msg + signature
 
-                # print ""
-                # print "sending final key message to: " + user
-                # print "this is the message:"
-                # print final_msg
-                # print ""
                 self.process_outgoing_message(final_msg, setup_message=True)
 
+    def get_pub_keys_from_files(self, user_list):
+        for user in user_list:
+            public_key_file = open("pub_key_" + user, "r")
+            pub_key = RSA.importKey(public_key_file.read())
+            self.public_keys[user] = pub_key
 
     def process_incoming_message(self, msg_raw, msg_id, owner_str):
 
         if msg_id in self.key_messages:
             return
-
-        if self.conversation_key != None:
-            print str(self.manager.user_name) + "'s conversation key is " + self.conversation_key
-
 
         '''
         Process incoming messages
@@ -207,8 +195,6 @@ class Conversation:
         :return: None
         '''
 
-        # print msg_id
-
         keystring = self.conversation_key
 
         chars = msg_raw[:2]
@@ -216,56 +202,49 @@ class Conversation:
 
 
         if chars != SETUP:
+            # Process a normal message
 
-
-            # # process message here
-            # # example is base64 decoding, extend this with any crypto processing of your protocol
-            #
-            # len_msg + header + ("0" * AES.block_size) + encrypted + mac
+            # Length of the message
             len_msg = msg_raw[:16]
             int_len_msg = int(len_msg)
-            print "len message = " + len_msg
+
+            # Get timestamp
             timestamp = msg_raw[16:42]
-            print "timestamp: " + timestamp
+
+            # Get message id
             msg_id = msg_raw[42:53]
-            print "msg id: " + msg_id
-            nonce = msg_raw[53: 53+ 8]
-            iv = "0" * AES.block_size
-            print "nonce: " + nonce
-            # print str(self.recent_msg_ids)
+
+            # Check for replay:
             if msg_id in self.recent_msg_ids:
-
-                print "message rejected, it is replayed"
-
-
+                # message rejected, it is replayed
+                return
             else:
+                # add message id to list of accepted message ids
                 self.recent_msg_ids[msg_id] = timestamp
 
+            header = timestamp + msg_id
 
+            # Get nonce
+            nonce = msg_raw[53: 53+ 8]
+
+            # Obtain encrypted message
             enc_msg = msg_raw[53+ 8: 53+ 8 + int_len_msg]
-            # print "encrypted msg : " + enc_msg
+
+            # Obtain MAC value
             rec_mac = msg_raw[53+ 8 + int_len_msg:]
-            print " rec mac: " + rec_mac
 
-
-
-
-            # intialize counter with the value read
+            # Initialize counter with the nonce value read
             iv = "0" * AES.block_size
             ctr = Counter.new(64, prefix=nonce, initial_value=0)
 
             # create AES cipher object
             cipher = AES.new(keystring, AES.MODE_CTR, counter=ctr)
 
+            # Decrypt the message
             decrypted_msg = cipher.decrypt(enc_msg)
-            print "decrypted: " + decrypted_msg
 
-            # generate mac from message
-
-            #pad msg if needed, padding sheme is x01 x00 ... x00
-
-            header = timestamp + msg_id
-
+            # generate MAC from decrypted message
+            # pad msg if needed, padding scheme is x01 x00 ... x00
             total_data = header + decrypted_msg
 
             p_length = AES.block_size - (len(total_data)) % AES.block_size
@@ -277,28 +256,25 @@ class Conversation:
                 total_data = total_data + chr(0)
                 p_length -= 1
 
-            # append message blocks X1...n and compute the MAC (as last block of CBC encryption of A|X)
+            # Append message blocks X1...n and compute the MAC (as last block of CBC encryption of A|X)
 
             # create AES CBC cipher object
             cbc_cipher = AES.new(keystring, AES.MODE_CBC, iv)
             total_encrypted_data = cbc_cipher.encrypt(total_data)
 
             mac = total_encrypted_data[-1 * AES.block_size:]
-            print "mac should be last block: "
-            print mac
-            # this should be further encrypted by XORing E_K(N|Ctr0) to it
-            # (where E_K() is ECB encryption of the single block N|ctr(0)
+
+            # Further encrypt MAC by XORing E_K(N|Ctr0) to it, (where E_K() is ECB encryption of the single block N|ctr(0)
             ecb_cipher = AES.new(keystring, AES.MODE_ECB)
 
+            # Pad the nonce a with block of 0s
             enc_nonce = ecb_cipher.encrypt(nonce + (chr(0) * 8))
 
             mac = self.xor_two_str(mac, enc_nonce)
 
-            print "xored mac: " + mac
-
             accepted = True
 
-            # check if received mac = mac generated
+            # check if received MAC = MAC generated
             i = 0
             while i < len(rec_mac) - 1:
                 if mac[i] != rec_mac[i]:
@@ -306,52 +282,59 @@ class Conversation:
                 i = i + 1
 
             if accepted:
-                # print message and add it to the list of printed messages
+                # Print message and add it to the list of printed messages
                 self.print_message (
                     msg_raw=decrypted_msg,
                     owner_str=owner_str
                 )
 
             else:
-                print "mac was rejected for message: " + decrypted_msg
+                # MAC was rejected
+                return
 
         elif chars == SETUP:
+            # Process a key exchange message
 
             timestamp = msg_raw[:26]
+
+            # Don't process a key message that was sent before we logged in
             if self.is_time_earlier(timestamp, str(self.my_login_time)):
                 return
 
-
-            print "processing setup message"
-            print msg_raw
+            # Add message id to list of key setup messages already processed,
+            # so we don't process it again during this conversation
             self.key_messages.append(msg_id)
 
+
+            # Don't process key setup message that we generated
             if (str(owner_str) != str(self.manager.user_name)):
-                # print "got setup message"
+
                 timestamp = msg_raw[:26]
                 msg_raw = msg_raw[26:]
                 enc_msg = msg_raw[:172]
                 signed_data = msg_raw[172:]
-
                 pubkey = self.public_keys[str(owner_str)]
                 verifier = PKCS1_PSS.new(pubkey)
+
+                # Data to sign: (B | Ta | Enc(A|K))
                 cert_data = str(self.manager.user_name) + timestamp + enc_msg
 
                 h = SHA.new()
                 h.update(self.md5(cert_data))
 
+                # Check that received signature is valid and timestamp is valid
                 if verifier.verify(h, signed_data):
                     valid_sig = True
                 else:
                     valid_sig = False
 
-                if valid_sig:
-                    # set key
-                    # print("conversation key received")
+                if valid_sig and self.is_time_earlier(str(self.curr_time), str(timestamp)):
+                    # This is a valid key exchange, we can decrypt and obtain session key
 
-                    # decrypt and obtain session key
+                    # Update this timestamp as most recent timestamp received
+                    self.curr_time = timestamp
 
-                    # encrypted message = (sender's name + session key) encrypted with my public key
+                    # Encrypted message = (sender's name + session key) encrypted with my public key
 
                     my_key_file = open("priv_key_" + str(self.manager.user_name), "r")
                     my_private_key = RSA.importKey(my_key_file.read())
@@ -361,18 +344,14 @@ class Conversation:
                     decrypted_message = cipher.decrypt(decoded_msg)
 
 
-                    # check if first part of decrypted message contains sender's name
-                    # if it is, the rest of the message contains the session key
-
+                    # Check if first part of decrypted message contains sender's name
+                    # If it is, the rest of the message contains the session key
                     if decrypted_message[:len(str(owner_str))] == str(owner_str):
                         self.conversation_key = decrypted_message[len(str(owner_str)):]
-                        print "session key is " + self.conversation_key
-
-                    # TODO: Check timestamp?
 
                     return
                 else:
-                    print("invalid key exchange")
+                    # This is an invalid key exchange
                     return
 
 
@@ -381,19 +360,14 @@ class Conversation:
         if not setup_message:
             '''
              Process an outgoing message before Base64 encoding
-
-           :param msg_raw: raw message
-            :return: message to be sent to the server
+                :param msg_raw: raw message
+                :return: message to be sent to the server
+                
             '''
-
             # first initialize N = random and ctr = 0
-            # then put timestamp and other stuff we dont want to encrypt in blocks A1...m
+            # then put timestamp and associated data we don't want to encrypt in blocks A1...m
 
-            print "processing outgoing message from " + str(self.manager.user_name) + ": " + msg_raw
-            print "using session key " + self.conversation_key
             nonce = get_random_bytes(8)
-            print "generated nonce: " + str(nonce)
-
             keystring = self.conversation_key
 
             # if the message has been typed into the console, record it, so it is never printed again during chatting
@@ -407,16 +381,19 @@ class Conversation:
 
 
             msg = msg_raw
+
+            # Generate random message id
             msg_id = get_random_bytes(11)
-            # print "msg id: " + msg_id
+
+            # Get current timestamp
             timestamp = str(datetime.datetime.now())
 
-            # print "timestamp: " + str(timestamp)
             header = str(timestamp) + msg_id
 
+            # Data being encrypted: (A|X)
             total_data = header + msg
 
-            # pad the information being encyrpted in CBC mode for the MAC
+            # Pad the information being encyrpted in CBC mode for the MAC
 
             p_length = AES.block_size - (len(total_data)) % AES.block_size
 
@@ -429,72 +406,42 @@ class Conversation:
 
             # append message blocks X1...n and compute the MAC (as last block of CBC encryption of A|X)
 
-
-
-            print "A|X: " + total_data
-
             # create AES CBC cipher object
             cbc_cipher = AES.new(keystring, AES.MODE_CBC, "0" * AES.block_size)
+
+            # Encrypt (A|X) in AEC CBC mode
             total_encrypted_data = cbc_cipher.encrypt(total_data)
-            print "Enc(A|X): "
-            print total_encrypted_data
+
+            # Obtain the MAC from the last block of the encrypted data
             mac = total_encrypted_data[-1 * AES.block_size:]
-            print "mac should be last block: "
-            print mac
+
             # this should be further encrypted by XORing E_K(N|Ctr0) to it
             # (where E_K() is ECB encryption of the single block N|ctr(0)
             ecb_cipher = AES.new(keystring, AES.MODE_ECB)
-
             enc_nonce = ecb_cipher.encrypt(nonce + chr(0)*8)
-
             mac = self.xor_two_str(mac, enc_nonce)
 
-            print "xored mac: " + mac
-            # print "mac created: " + mac
-            # print "length of mac: " + str(len(mac))
-
+            # Pad length of message into a 16 bit block
             len_msg = str(len(msg))
             while len(len_msg) < 16:
                 len_msg = "0" + len_msg
 
-            # print "len msg = " + len_msg
-
             # Finally encrypt in CTR mode the blocks X1...n and append the encrypted MAC to get the final output
 
             # initialize CTR mode, encrypt message
-            iv = "0" * AES.block_size
             ctr = Counter.new(64, prefix=nonce, initial_value=0)
             ctr_cipher = AES.new(keystring, AES.MODE_CTR, counter=ctr)
 
-            # p_length = AES.block_size - (len(msg)) % AES.block_size
-            #
-            # if p_length >= 1:
-            #     msg = msg + chr(1)
-            #     p_length -= 1
-            # while p_length > 0:
-            #     msg = msg + chr(0)
-            #     p_length -= 1
-
-            print "message length is: " + str(len(msg))
+            # Encrypt the message blocks
             encrypted = ctr_cipher.encrypt(msg)
 
-            print "encrypted message: " + encrypted
-            #
-            # print "length of msg enc: " + str(len(encrypted))
-
-            # format message header
-
-
-            # get final message
-
+            # Get final message (00 | length of message | nonce | Enc(msg) | MAC)
             encoded_msg = NORMAL + len_msg + header + nonce + encrypted + mac
-
-            print "message to send: " + encoded_msg
 
             msg_to_send = encoded_msg
 
         else:
-            # append 11 to setup messages
+            # append "11" to setup messages
             msg_to_send = SETUP + msg_raw
 
 
@@ -552,8 +499,8 @@ class Conversation:
         return h.digest()
 
 
-    # returns true if t1 is earlier than t2
-    # returns false otherwise
+    # returns True if t1 is earlier than t2
+    # returns False otherwise
     def is_time_earlier(self, t1, t2):
         timestamp1 = datetime.datetime.strptime(t1, "%Y-%m-%d %H:%M:%S.%f")
         timestamp2 = datetime.datetime.strptime(t2, "%Y-%m-%d %H:%M:%S.%f")
